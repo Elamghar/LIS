869907a69d389f4d6bb191347dd2cea1d7fbb30f
Elamghar <161138891+Elamghar@users.noreply.github.com>
2024-11-13 00:31:26 +0100

    ajout d Dao et DaoImp et  exceptions

--- src/main/java/ma/ensa/lis/Dao/Impl/VisitDaoImp.java
+++ src/main/java/ma/ensa/lis/Dao/Impl/VisitDaoImp.java
@@ -1,4 +1,50 @@
 package ma.ensa.lis.Dao.Impl;
 
-public class VisitDaoImp {
+import ma.ensa.lis.Dao.VisitDao;
+import ma.ensa.lis.models.Visit;
+import ma.ensa.lis.utils.DbConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class VisitDaoImp implements VisitDao {
+    @Override
+    public void save(Visit v) {
+        DbConnection db = new DbConnection();
+        Connection conn = db.getConn();
+        String query = "INSERT INTO Visit (visitId,visitDate,diagnostic) VALUES (?, ?,?)";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, v.getId());
+            stmt.setString(2, v.getDate());
+            stmt.setString(2, v.getDiagnostic());
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                // Login Successful if match is found
+                boolean success = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            //rs.close();
+
+        }
+    }
+
+    @Override
+    public void delete(Visit v) {
+
+    }
+
+    @Override
+    public Visit search(String id) {
+        return null;
+    }
+
+    @Override
+    public Visit search(Date dateVisit) {
+        return null;
+    }
 }
