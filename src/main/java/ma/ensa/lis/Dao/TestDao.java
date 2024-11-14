19c561f657d6a77b5489b10685cdeb8c1f95c474
Othmane Abderrazik <121362196+OthmaneAbder2303@users.noreply.github.com>
2024-11-14 18:45:16 +0100

    Update TestDao.java

--- src/main/java/ma/ensa/lis/Dao/TestDao.java
+++ src/main/java/ma/ensa/lis/Dao/TestDao.java
@@ -1,4 +1,15 @@
 package ma.ensa.lis.Dao;
 
+import ma.ensa.lis.models.Test;
+import java.util.List;
+
 public interface TestDao {
+
+    Test findById(String id);
+    List<Test> findAll();
+    void save(Test test);
+    void update(Test test);
+    void delete(String id);
+
 }
+
