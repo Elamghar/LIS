18099dc11c5c26f95617b269ccf1e9e0433a8bb0
Elamghar <161138891+Elamghar@users.noreply.github.com>
2024-11-15 00:38:39 +0100

    qlq mise a jour

--- src/main/java/ma/ensa/lis/models/Visit.java
+++ src/main/java/ma/ensa/lis/models/Visit.java
@@ -1,9 +1,14 @@
 package ma.ensa.lis.models;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ma.ensa.lis.Exceptions.TestException;
 import ma.ensa.lis.Exceptions.patientException;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,23 +18,26 @@ public class Visit {
     private String visitId;
     private Date visitDate;
     private String diagnostic;
-    //private List<Test> tests ;
-    //ajout d'un staff
+    private List<Test> tests ;
     Patient p;
+
+
 
     public Visit(String visitId,Date visitDate){
         this.visitId=visitId;
         this.visitDate=visitDate;
         //p=new Patient();
     }
-
+    void addTest(Test test) throws  TestException {
+        if(test==null)throw new TestException("Instance null in Visit.addTest");
+    }
     void addPatient(Patient p) throws patientException {
      if(p==null)throw new patientException("Instance Patient is null");
      this.p=p;
     }
-
     public  String toJson(){
         //implementation to facilate Xml manip
         return null;
     }
+
 }
