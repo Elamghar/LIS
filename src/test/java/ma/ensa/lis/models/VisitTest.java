057531511e1e8065e5697b72a27602c54a836fdf
Elamghar <161138891+Elamghar@users.noreply.github.com>
2024-11-21 22:41:22 +0100

    Add target directory to .gitignore

--- src/test/java/ma/ensa/lis/models/VisitTest.java
+++ src/test/java/ma/ensa/lis/models/VisitTest.java
@@ -1,30 +1,4 @@
-package ma.ensa.lis.models;
-
-import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
 class VisitTest {
-
-    @Test
-    void getVisitId() {
-    }
-
-    @Test
-    void getVisitDate() {
-    }
-
-    @Test
-    void setVisitId() {
-    }
-
-    @Test
-    void setVisitDate() {
-    }
-
-    @Test
-    void addPatient() {
-    }
-
-    @Test
-    void toJson() {
-    }
+  
 }
