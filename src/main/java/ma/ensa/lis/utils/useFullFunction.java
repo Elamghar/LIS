77474e4a97aeac8e61d5d87a300185e8a4d91756
Elamghar <161138891+Elamghar@users.noreply.github.com>
2024-11-21 22:44:15 +0100

    the pull and add target to gitignore

--- src/main/java/ma/ensa/lis/utils/useFullFunction.java
+++ src/main/java/ma/ensa/lis/utils/useFullFunction.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 
 public class useFullFunction {
+
     public static void executeSqlScript(Connection conn, String filePath) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath));
              Statement stmt = conn.createStatement()) {
