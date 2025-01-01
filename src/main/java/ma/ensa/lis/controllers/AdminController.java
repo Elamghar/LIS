88d2d189661a2a97adbee33436cebeb1ec9e6a56
Elamghar <161138891+Elamghar@users.noreply.github.com>
2025-01-01 18:50:51 +0100

    ajout de selection et les icons

--- src/main/java/ma/ensa/lis/controllers/AdminController.java
+++ src/main/java/ma/ensa/lis/controllers/AdminController.java
@@ -96,8 +96,8 @@ public class AdminController implements Initializable {
     public void createPatient(javafx.event.ActionEvent actionEvent) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ma/ensa/lis/AjoutPatient-view.fxml"));
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        Scene scene = new Scene(fxmlLoader.load(), 600, 650);
-        String css = Objects.requireNonNull(this.getClass().getResource("/ma/ensa/lis/STYLE.css")).toExternalForm();
+        Scene scene = new Scene(fxmlLoader.load(), 620, 650);
+        String css = Objects.requireNonNull(this.getClass().getResource("/ma/ensa/lis/ajoutpa_style.css")).toExternalForm();
         scene.getStylesheets().add(css);
         stage.setTitle("LIS");
         stage.setScene(scene);
